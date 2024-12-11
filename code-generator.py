6104f121ded5e94fb56fbd47b98bd94833506f72
dev.realbits <dev.realbits@gmail.com>
2024-12-11 22:58:39 +0900

    Merge the code generator into a single file

--- code-generator.py
+++ code-generator.py
@@ -1,7 +1,6 @@
 import autogen
 import os
 from dotenv import load_dotenv
-import json
 from autogen.coding import LocalCommandLineCodeExecutor
 
 load_dotenv()
@@ -33,10 +32,10 @@ initializer = autogen.UserProxyAgent(
     },
 )
 
-html_code_generator = autogen.AssistantAgent(
-    name="HTML_Code_Generator",
+code_generator = autogen.AssistantAgent(
+    name="Code_Generator",
     description="""
-    HTML code generator to meet the requirement.
+    Code generator to meet the requirement.
     """,
     llm_config=gpt4_config,
     system_message="""
@@ -62,10 +61,10 @@ html_code_generator = autogen.AssistantAgent(
     """,
 )
 
-html_code_executor = autogen.UserProxyAgent(
-    name="HTML_Code_Executor",
+code_executor = autogen.UserProxyAgent(
+    name="Code_Executor",
     description="""
-    Code executor to run a HTML code written by the Coder and
+    Code executor to run a code written by the Coder and
     report the result.
     """,
     system_message="""
@@ -76,72 +75,8 @@ html_code_executor = autogen.UserProxyAgent(
     code_execution_config={"executor": executor},
 )
 
-html_code_reviewer = autogen.AssistantAgent(
-    name="HTML_Code_Reviewer",
-    description="""
-    Code reviewer to review the code written by the Coder and
-    make sure it's correct.
-    """,
-    llm_config=gpt4_config,
-    system_message="""
-    You are the code reviewer.
-    Please review the code written by the Coder and make sure it's correct.
-    If it's not correct, fix it and output the code again.
-    Suggest the full code instead of partial code or code changes.
-    If the error can't be fixed or if the task is not solved
-    even after the code is executed successfully,
-    analyze the problem, revisit your assumption,
-    collect additional info you need, and
-    think of a different approach to try.
-    If you think the task is solved, output only "OK".
-    """,
-)
-
-python_code_generator = autogen.AssistantAgent(
-    name="Python_Code_Generator",
-    description="""
-    Python code generator to meet the requirement.
-    """,
-    llm_config=gpt4_config,
-    system_message="""
-    You are the Coder. Given a topic,
-    write the code to meet the topic.
-    You write the code to solve tasks.
-    Wrap the code in a code block that specifies the script type.
-    The user can't modify your code.
-    So do not suggest incomplete code which requires others to modify.
-    Don't use a code block
-    if it's not intended to be executed by the executor.
-    Don't include multiple code blocks in one response.
-    Do not ask others to copy and paste the result.
-    Check the execution result returned by the executor.
-    If the result indicates there is an error,
-    fix the error and output the code again.
-    Suggest the full code instead of partial code or code changes.
-    If the error can't be fixed or if the task is not solved
-    even after the code is executed successfully,
-    analyze the problem, revisit your assumption,
-    collect additional info you need, and
-    think of a different approach to try.
-    """,
-)
-
-python_code_executor = autogen.UserProxyAgent(
-    name="Python_Code_Executor",
-    description="""
-    Code executor to execute a python code written by the Coder and
-    report the result.
-    """,
-    system_message="""
-    Execute the code written by the Coder and
-    report the result.
-    """,
-    human_input_mode="NEVER",
-    code_execution_config={"executor": executor},
-)
-
-python_code_reviewer = autogen.AssistantAgent(
-    name="Python_Code_Reviewer",
+code_reviewer = autogen.AssistantAgent(
+    name="Code_Reviewer",
     description="""
     Code reviewer to review the code written by the Coder and
     make sure it's correct.
@@ -167,45 +102,23 @@ def state_transition(last_speaker, groupchat):
 
     # Initial state
     if last_speaker is initializer:
-        return html_code_generator
+        return code_generator
     # HTML code generation
-    elif last_speaker is html_code_generator:
-        return html_code_executor
+    elif last_speaker is code_generator:
+        return code_executor
     # HTML code execution
-    elif last_speaker is html_code_executor:
-        return html_code_reviewer
+    elif last_speaker is code_executor:
+        return code_reviewer
     # HTML code review
-    elif last_speaker is html_code_reviewer:
-        if messages[-1]["content"] == "OK":
-            return python_code_generator
-        else:
-            return html_code_generator
-    # Python code generation
-    elif last_speaker is python_code_generator:
-        return python_code_executor
-    # Python code execution
-    elif last_speaker is python_code_executor:
-        if messages[-1]["content"] == "exitcode: 1":
-            return python_code_generator
-        else:
-            return python_code_reviewer
-    # Python code review
-    elif last_speaker is python_code_reviewer:
-        print(json.dumps(messages, indent=2, ensure_ascii=False))
+    elif last_speaker is code_reviewer:
         if messages[-1]["content"] == "OK":
             return None
         else:
-            return python_code_generator
+            return code_generator
 
 
 groupchat = autogen.GroupChat(
-    agents=[initializer,
-            html_code_generator,
-            html_code_executor,
-            html_code_reviewer,
-            python_code_generator,
-            python_code_executor,
-            python_code_reviewer],
+    agents=[initializer, code_generator, code_executor, code_reviewer],
     messages=[],
     max_round=20,
     speaker_selection_method=state_transition,
@@ -219,7 +132,7 @@ initializer.initiate_chat(
 
     Requirement:
 
-    # Generate HTML code and run it.
+    # Generate HTML code
 
     Generate these items as an HTML file.
 
@@ -235,10 +148,9 @@ initializer.initiate_chat(
     - Contact: List the contact information of the product.
       * ai.thothy@gmail.com
 
-    - No sign up/in form. """ +
-    # - Generate each page as an one HTML file.
+    - No sign up/in form.
+    - Generate each page as an one HTML file.
 
-    """
     - The landing page is made by HTML, CSS, and JavaScript.
     - The landing page should be a single page.
     - The landing page should be responsive.
