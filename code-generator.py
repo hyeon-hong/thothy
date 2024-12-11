17965630634fc26e3f65ad4280162742a36d798c
dev.realbits <dev.realbits@gmail.com>
2024-12-11 19:52:43 +0900

    Remove unnecessary comments

--- code-generator.py
+++ code-generator.py
@@ -34,24 +34,24 @@ code_generator = autogen.AssistantAgent(
     """,
     llm_config=gpt4_config,
     system_message="""
-    You are the Coder. Given a topic, \
-    write the code to meet the topic. \
-    You write the code to solve tasks. \
-    Wrap the code in a code block that specifies the script type. \
-    The user can't modify your code. \
-    So do not suggest incomplete code which requires others to modify. \
-    Don't use a code block \
-    if it's not intended to be executed by the executor. \
-    Don't include multiple code blocks in one response. \
-    Do not ask others to copy and paste the result. \
-    Check the execution result returned by the executor. \
-    If the result indicates there is an error, \
-    fix the error and output the code again. \
-    Suggest the full code instead of partial code or code changes. \
-    If the error can't be fixed or if the task is not solved \
-    even after the code is executed successfully, \
-    analyze the problem, revisit your assumption, \
-    collect additional info you need, and \
+    You are the Coder. Given a topic,
+    write the code to meet the topic.
+    You write the code to solve tasks.
+    Wrap the code in a code block that specifies the script type.
+    The user can't modify your code.
+    So do not suggest incomplete code which requires others to modify.
+    Don't use a code block
+    if it's not intended to be executed by the executor.
+    Don't include multiple code blocks in one response.
+    Do not ask others to copy and paste the result.
+    Check the execution result returned by the executor.
+    If the result indicates there is an error,
+    fix the error and output the code again.
+    Suggest the full code instead of partial code or code changes.
+    If the error can't be fixed or if the task is not solved
+    even after the code is executed successfully,
+    analyze the problem, revisit your assumption,
+    collect additional info you need, and
     think of a different approach to try.
     """,
 )
@@ -59,11 +59,11 @@ code_generator = autogen.AssistantAgent(
 code_executor = autogen.UserProxyAgent(
     name="Code_Executor",
     description="""
-    Code executor to execute the code written by the Coder and \
+    Code executor to execute the code written by the Coder and
     report the result.
     """,
     system_message="""
-    Execute the code written by the Coder and \
+    Execute the code written by the Coder and
     report the result.
     """,
     human_input_mode="NEVER",
@@ -78,19 +78,19 @@ code_executor = autogen.UserProxyAgent(
 code_reviewer = autogen.AssistantAgent(
     name="Code_Reviewer",
     description="""
-    Code reviewer to review the code written by the Coder and \
+    Code reviewer to review the code written by the Coder and
     make sure it's correct.
     """,
     llm_config=gpt4_config,
     system_message="""
-    You are the code reviewer. \
-    Please review the code written by the Coder and make sure it's correct. \
-    If it's not correct, fix it and output the code again. \
-    Suggest the full code instead of partial code or code changes. \
-    If the error can't be fixed or if the task is not solved \
-    even after the code is executed successfully, \
-    analyze the problem, revisit your assumption, \
-    collect additional info you need, and \
+    You are the code reviewer.
+    Please review the code written by the Coder and make sure it's correct.
+    If it's not correct, fix it and output the code again.
+    Suggest the full code instead of partial code or code changes.
+    If the error can't be fixed or if the task is not solved
+    even after the code is executed successfully,
+    analyze the problem, revisit your assumption,
+    collect additional info you need, and
     think of a different approach to try.
     """,
 )
@@ -129,12 +129,14 @@ manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
 initializer.initiate_chat(
     manager,
     message="""
-    Topic: Generate a landing page for a SaaS product that builds AI agents. \
-    Requirement:  \
-    - The landing page is made by HTML, CSS, and JavaScript. \
-    - The landing page should be a single page. \
-    - The landing page should be responsive. \
-    - The landing page should be mobile friendly. \
+    Topic: Generate a landing page for a SaaS product that builds AI agents
+    and save each page as a separate HTML file.
+
+    Requirement:
+    - The landing page is made by HTML, CSS, and JavaScript.
+    - The landing page should be a single page.
+    - The landing page should be responsive.
+    - The landing page should be mobile friendly.
     - The landing page should be desktop friendly.
     """,
 )
