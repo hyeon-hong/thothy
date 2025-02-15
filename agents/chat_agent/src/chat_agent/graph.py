ed9d026a0cc36d45b08e5baa41c4990852cd664f
realbits-lab <dev.realbits@gmail.com>
2025-02-15 22:26:56 +0900

    Add memory to chat agent

--- agents/chat_agent/src/chat_agent/graph.py
+++ agents/chat_agent/src/chat_agent/graph.py
@@ -1,22 +1,49 @@
 """Simple chat agent using LangGraph."""
 
 import os
+import uuid
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+
 from langchain_core.runnables import RunnableConfig
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState, StateGraph, START, END
+from langgraph.store.memory import InMemoryStore
+from langgraph.store.base import BaseStore
+
 
 load_dotenv()
 llm = ChatOpenAI(model="gpt-4o-mini", api_key=os.getenv("OPENAI_API_KEY"))
+in_memory_store = InMemoryStore(
+    index={
+        "embed": OpenAIEmbeddings(model="text-embedding-3-small"),
+        "dims": 1536,
+    }
+)
 
 
-def chatbot(state: MessagesState, config: RunnableConfig) -> dict:
+def chatbot(state: MessagesState, config: RunnableConfig, *, store: BaseStore) -> dict:
     """Chat node that processes messages and generates responses."""
     user_id = config["configurable"]["user_id"]
-    print(f"user_id: {user_id}")
+    # print(f"user_id: {user_id}")
+    namespace = ("memories", user_id)
+    memories = store.search(namespace, query=str(
+        state["messages"][-1].content))
+    info = "\n".join([d.value["data"] for d in memories])
+    system_msg = f"You are a helpful assistant talking to the user. User info: {info}"
 
-    # Invoke LLM with current messages and return response
-    return {"messages": [llm.invoke(state["messages"])]}
+    # Store new memories if the user asks the model to remember
+    last_message = state["messages"][-1]
+    if "remember" in last_message.content.lower():
+        memory = "User name is Bob"
+        store.put(namespace, str(uuid.uuid4()), {"data": memory})
+
+    response = llm.invoke(
+        [{"role": "system", "content": system_msg}] + state["messages"]
+    )
+    return {"messages": response}
 
 
 def build_chat_graph():
@@ -32,7 +59,7 @@ def build_chat_graph():
     graph_builder.add_edge("chatbot", END)
 
     # Compile graph
-    return graph_builder.compile()
+    return graph_builder.compile(checkpointer=MemorySaver(), store=in_memory_store)
 
 
 # Create the graph
