a1b69e60ecda458ff12f5a5d0cd08e8aa578181a
realbits-lab <dev.realbits@gmail.com>
2025-03-17 18:44:35 +0900

    refactor: Update team graph to replace researcher and coder with news_agent and blog_agent, removing unused tools and enhancing logging

---
+++ backend/team_agent/README.md
@@ -0,0 +1,24 @@
+# Team Agent
+
+The team agent is a supervisor that can manage a conversation between multiple agents.
+
+It is a simple supervisor that can manage a conversation between multiple agents.
+
+## Usage
+
+```python
+from team_graph.graph import graph
+```
+
+## Concepts
+
+### Agents
+
+Use Supervisor to manage a conversation between multiple agents.
+https://langchain-ai.github.io/langgraph/concepts/multi_agent/#supervisor
+https://langchain-ai.github.io/langgraph/tutorials/multi_agent/agent_supervisor/
+
+### Subgraphs as a function
+
+Use the compiled graph as a function.
+https://langchain-ai.github.io/langgraph/concepts/low_level/#as-a-function
--- backend/team_agent/src/team_graph/graph.py
+++ backend/team_agent/src/team_graph/graph.py
@@ -1,38 +1,14 @@
-from langgraph.prebuilt import create_react_agent
-from langgraph.graph import StateGraph, START, END
-from langchain_core.messages import HumanMessage
+import logging
+from langgraph.graph import MessagesState, StateGraph, START, END
 from langgraph.types import Command
-from langgraph.graph import MessagesState, END
 from langchain_anthropic import ChatAnthropic
 from typing_extensions import TypedDict
 from typing import Literal
-from typing import Annotated
 
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.tools import tool
-from langchain_experimental.utilities import PythonREPL
+from blog_graph.graph import graph as blog_graph
+from news_graph.graph import graph as news_graph
 
-tavily_tool = TavilySearchResults(max_results=5)
-
-# This executes code locally, which can be unsafe
-repl = PythonREPL()
-
-
-@tool
-def python_repl_tool(
-    code: Annotated[str, "The python code to execute to generate your chart."],
-):
-    """Use this to execute python code and do math. If you want to see the output of a value,
-    you should print it out with `print(...)`. This is visible to the user."""
-    try:
-        result = repl.run(code)
-    except BaseException as e:
-        return f"Failed to execute. Error: {repr(e)}"
-    result_str = f"Successfully executed:\n\`\`\`python\n{code}\n\`\`\`\nStdout: {result}"
-    return result_str
-
-
-members = ["researcher", "coder"]
+members = ["news_agent", "blog_agent"]
 # Our team supervisor is an LLM node. It just picks the next agent to process
 # and decides when the work is completed
 options = members + ["FINISH"]
@@ -55,15 +31,23 @@ class Router(TypedDict):
 llm = ChatAnthropic(model="claude-3-5-sonnet-latest")
 
 
-class State(MessagesState):
-    next: str
+async def call_news_agent(state: MessagesState):
+    """Call the news agent with the current state."""
+    return await news_graph.invoke(state)
 
 
-def supervisor_node(state: State) -> Command[Literal[*members, "__end__"]]:
+async def call_blog_agent(state: MessagesState):
+    """Call the blog agent with the current state."""
+    return await blog_graph.invoke(state)
+
+
+def supervisor_node(state: MessagesState) -> Command[Literal[*members, "__end__"]]:
     messages = [
         {"role": "system", "content": system_prompt},
     ] + state["messages"]
+    logging.info(f"Supervisor messages: {messages}")
     response = llm.with_structured_output(Router).invoke(messages)
+    logging.info(f"Supervisor response: {response}")
     goto = response["next"]
     if goto == "FINISH":
         goto = END
@@ -71,44 +55,19 @@ def supervisor_node(state: State) -> Command[Literal[*members, "__end__"]]:
     return Command(goto=goto, update={"next": goto})
 
 
-research_agent = create_react_agent(
-    llm, tools=[tavily_tool], prompt="You are a researcher. DO NOT do any math."
-)
+# Build the graph
+builder = StateGraph(MessagesState)
 
-
-def research_node(state: State) -> Command[Literal["supervisor"]]:
-    result = research_agent.invoke(state)
-    return Command(
-        update={
-            "messages": [
-                HumanMessage(content=result["messages"]
-                             [-1].content, name="researcher")
-            ]
-        },
-        goto="supervisor",
-    )
-
-
-# NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION, WHICH CAN BE UNSAFE WHEN NOT SANDBOXED
-code_agent = create_react_agent(llm, tools=[python_repl_tool])
-
-
-def code_node(state: State) -> Command[Literal["supervisor"]]:
-    result = code_agent.invoke(state)
-    return Command(
-        update={
-            "messages": [
-                HumanMessage(content=result["messages"]
-                             [-1].content, name="coder")
-            ]
-        },
-        goto="supervisor",
-    )
-
-
-builder = StateGraph(State)
+# Add edges
 builder.add_edge(START, "supervisor")
+
+# Add nodes
 builder.add_node("supervisor", supervisor_node)
-builder.add_node("researcher", research_node)
-builder.add_node("coder", code_node)
+builder.add_node("news_agent", call_news_agent)
+builder.add_node("blog_agent", call_blog_agent)
+
+# Compile the graph
 graph = builder.compile()
+graph.name = "team_graph"
+
+__all__ = ["graph"]
