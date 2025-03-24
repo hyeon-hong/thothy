d7d578417d29068c6e2828e4768f53779f0d2381
realbits-lab <dev.realbits@gmail.com>
2025-03-24 22:18:25 +0900

    refactor: simplify ChatAgentPage structure by removing unnecessary routing components and enhancing readability

--- frontend/src/app/agents/chat_agent/page.tsx
+++ frontend/src/app/agents/chat_agent/page.tsx
@@ -3,20 +3,16 @@ import { Thread } from "@/components/thread";
 import { StreamProvider } from "@/providers/Stream";
 import { ThreadProvider } from "@/providers/Thread";
 import { Toaster } from "@/components/ui/sonner";
-import { NuqsAdapter } from "nuqs/adapters/react-router/v6";
-import { BrowserRouter } from "react-router-dom";
 
 export default function ChatAgentPage() {
   return (
-    <BrowserRouter>
-      <NuqsAdapter>
-        <ThreadProvider>
-          <StreamProvider>
-            <Thread />
-          </StreamProvider>
-        </ThreadProvider>
-        <Toaster />
-      </NuqsAdapter>
-    </BrowserRouter>
+    <>
+      <ThreadProvider>
+        <StreamProvider>
+          <Thread />
+        </StreamProvider>
+      </ThreadProvider>
+      <Toaster />
+    </>
   );
 }
