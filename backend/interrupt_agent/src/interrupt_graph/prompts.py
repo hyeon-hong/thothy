657a7bba4783ff18362b30073623b97b7a4d1500
realbits-lab <dev.realbits@gmail.com>
2025-04-16 10:47:50 +0900

    refactor: remove interrupt_agent components including configuration files, prompts, and graph definitions to streamline project structure and eliminate unused code

--- backend/interrupt_agent/src/interrupt_graph/prompts.py
+++
@@ -1,7 +0,0 @@
-"""Define default prompts."""
-
-SYSTEM_PROMPT = (
-    "You are a helpful and friendly chatbot. Get to know the user!"
-    " Ask questions! Be spontaneous!"
-    "\n\nSystem Time: {time}"
-)
